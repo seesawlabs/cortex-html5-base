0d526189c05ade26f34ee52d7d9ea418a0be9d2a
Calvin Sauer <calvin.j.sauer@gmail.com>
2017-06-01 19:33:00 -0400

    Include year in test data

--- src/test-data.js
+++ src/test-data.js
@@ -3,213 +3,14 @@
 const TEST_DATA = [
   {
 	"_index": "london",
-	"hour": 0,
-	"icon": "chancerain",
-	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 63,
-	"temp_metric": 17
-  },
-  {
-	"_index": "london",
-	"hour": 9,
-	"icon": "mostlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 66,
-	"temp_metric": 19
-  },
-  {
-	"_index": "london",
-	"hour": 12,
-	"icon": "mostlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 71,
-	"temp_metric": 22
-  },
-  {
-	"_index": "london",
-	"hour": 1,
-	"icon": "chancerain",
-	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 63,
-	"temp_metric": 17
-  },
-  {
-	"_index": "london",
-	"hour": 3,
-	"icon": "chancerain",
-	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 61,
-	"temp_metric": 16
-  },
-  {
-	"_index": "london",
-	"hour": 7,
-	"icon": "cloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 59,
-	"temp_metric": 15
-  },
-  {
-	"_index": "london",
-	"hour": 10,
-	"icon": "mostlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 69,
-	"temp_metric": 21
-  },
-  {
-	"_index": "london",
 	"hour": 13,
 	"icon": "mostlycloudy",
 	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
 	"mday": 2,
 	"mon": 6,
 	"temp_english": 72,
-	"temp_metric": 22
-  },
-  {
-	"_index": "london",
-	"hour": 21,
-	"icon": "cloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 68,
-	"temp_metric": 20
-  },
-  {
-	"_index": "london",
-	"hour": 20,
-	"icon": "cloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 70,
-	"temp_metric": 21
-  },
-  {
-	"_index": "london",
-	"hour": 2,
-	"icon": "clear",
-	"icon_url": "http://icons.wxug.com/i/c/k/nt_clear.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 61,
-	"temp_metric": 16
-  },
-  {
-	"_index": "london",
-	"hour": 18,
-	"icon": "cloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 72,
-	"temp_metric": 22
-  },
-  {
-	"_index": "london",
-	"hour": 5,
-	"icon": "partlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/partlycloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 55,
-	"temp_metric": 13
-  },
-  {
-	"_index": "london",
-	"hour": 8,
-	"icon": "mostlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 63,
-	"temp_metric": 17
-  },
-  {
-	"_index": "london",
-	"hour": 4,
-	"icon": "chancerain",
-	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 60,
-	"temp_metric": 16
-  },
-  {
-	"_index": "london",
-	"hour": 2,
-	"icon": "chancerain",
-	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 61,
-	"temp_metric": 16
-  },
-  {
-	"_index": "london",
-	"hour": 9,
-	"icon": "mostlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 60,
-	"temp_metric": 16
-  },
-  {
-	"_index": "london",
-	"hour": 8,
-	"icon": "mostlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 59,
-	"temp_metric": 15
-  },
-  {
-	"_index": "london",
-	"hour": 17,
-	"icon": "mostlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 73,
-	"temp_metric": 23
-  },
-  {
-	"_index": "london",
-	"hour": 6,
-	"icon": "cloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 59,
-	"temp_metric": 15
-  },
-  {
-	"_index": "london",
-	"hour": 7,
-	"icon": "mostlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 60,
-	"temp_metric": 16
+	"temp_metric": 22,
+	"year": 2017
   },
   {
 	"_index": "london",
@@ -219,97 +20,8 @@ const TEST_DATA = [
 	"mday": 2,
 	"mon": 6,
 	"temp_english": 73,
-	"temp_metric": 23
-  },
-  {
-	"_index": "london",
-	"hour": 11,
-	"icon": "chancerain",
-	"icon_url": "http://icons.wxug.com/i/c/k/chancerain.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 70,
-	"temp_metric": 21
-  },
-  {
-	"_index": "london",
-	"hour": 1,
-	"icon": "clear",
-	"icon_url": "http://icons.wxug.com/i/c/k/nt_clear.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 63,
-	"temp_metric": 17
-  },
-  {
-	"_index": "london",
-	"hour": 3,
-	"icon": "clear",
-	"icon_url": "http://icons.wxug.com/i/c/k/nt_clear.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 59,
-	"temp_metric": 15
-  },
-  {
-	"_index": "london",
-	"hour": 22,
-	"icon": "chancerain",
-	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 66,
-	"temp_metric": 19
-  },
-  {
-	"_index": "london",
-	"hour": 16,
-	"icon": "mostlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 73,
-	"temp_metric": 23
-  },
-  {
-	"_index": "london",
-	"hour": 19,
-	"icon": "cloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 71,
-	"temp_metric": 22
-  },
-  {
-	"_index": "london",
-	"hour": 5,
-	"icon": "cloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 59,
-	"temp_metric": 15
-  },
-  {
-	"_index": "london",
-	"hour": 6,
-	"icon": "partlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/partlycloudy.gif",
-	"mday": 2,
-	"mon": 6,
-	"temp_english": 57,
-	"temp_metric": 14
-  },
-  {
-	"_index": "london",
-	"hour": 10,
-	"icon": "partlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/partlycloudy.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 62,
-	"temp_metric": 17
+	"temp_metric": 23,
+	"year": 2017
   },
   {
 	"_index": "london",
@@ -319,37 +31,85 @@ const TEST_DATA = [
 	"mday": 2,
 	"mon": 6,
 	"temp_english": 65,
-	"temp_metric": 18
+	"temp_metric": 18,
+	"year": 2017
   },
   {
 	"_index": "london",
-	"hour": 11,
-	"icon": "partlycloudy",
-	"icon_url": "http://icons.wxug.com/i/c/k/partlycloudy.gif",
-	"mday": 3,
-	"mon": 6,
-	"temp_english": 63,
-	"temp_metric": 17
-  },
-  {
-	"_index": "london",
-	"hour": 0,
-	"icon": "clear",
-	"icon_url": "http://icons.wxug.com/i/c/k/nt_clear.gif",
+	"hour": 17,
+	"icon": "mostlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
 	"mday": 2,
 	"mon": 6,
-	"temp_english": 65,
-	"temp_metric": 18
+	"temp_english": 73,
+	"temp_metric": 23,
+	"year": 2017
   },
   {
 	"_index": "london",
-	"hour": 15,
+	"hour": 19,
 	"icon": "cloudy",
 	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
 	"mday": 2,
 	"mon": 6,
-	"temp_english": 73,
-	"temp_metric": 23
+	"temp_english": 71,
+	"temp_metric": 22,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 12,
+	"icon": "mostlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 71,
+	"temp_metric": 22,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 6,
+	"icon": "partlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/partlycloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 57,
+	"temp_metric": 14,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 2,
+	"icon": "clear",
+	"icon_url": "http://icons.wxug.com/i/c/k/nt_clear.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 61,
+	"temp_metric": 16,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 12,
+	"icon": "partlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/partlycloudy.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 64,
+	"temp_metric": 18,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 8,
+	"icon": "mostlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 63,
+	"temp_metric": 17,
+	"year": 2017
   },
   {
 	"_index": "london",
@@ -359,7 +119,283 @@ const TEST_DATA = [
 	"mday": 2,
 	"mon": 6,
 	"temp_english": 55,
-	"temp_metric": 13
+	"temp_metric": 13,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 5,
+	"icon": "partlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/partlycloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 55,
+	"temp_metric": 13,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 0,
+	"icon": "chancerain",
+	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 63,
+	"temp_metric": 17,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 22,
+	"icon": "chancerain",
+	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 66,
+	"temp_metric": 19,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 9,
+	"icon": "mostlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 66,
+	"temp_metric": 19,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 15,
+	"icon": "cloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 73,
+	"temp_metric": 23,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 21,
+	"icon": "cloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 68,
+	"temp_metric": 20,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 11,
+	"icon": "partlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/partlycloudy.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 63,
+	"temp_metric": 17,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 20,
+	"icon": "cloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 70,
+	"temp_metric": 21,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 2,
+	"icon": "chancerain",
+	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 61,
+	"temp_metric": 16,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 10,
+	"icon": "mostlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 69,
+	"temp_metric": 21,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 6,
+	"icon": "cloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 59,
+	"temp_metric": 15,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 8,
+	"icon": "mostlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 59,
+	"temp_metric": 15,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 5,
+	"icon": "cloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 59,
+	"temp_metric": 15,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 7,
+	"icon": "mostlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 60,
+	"temp_metric": 16,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 9,
+	"icon": "mostlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 60,
+	"temp_metric": 16,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 10,
+	"icon": "partlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/partlycloudy.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 62,
+	"temp_metric": 17,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 1,
+	"icon": "chancerain",
+	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 63,
+	"temp_metric": 17,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 7,
+	"icon": "cloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 59,
+	"temp_metric": 15,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 1,
+	"icon": "clear",
+	"icon_url": "http://icons.wxug.com/i/c/k/nt_clear.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 63,
+	"temp_metric": 17,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 11,
+	"icon": "chancerain",
+	"icon_url": "http://icons.wxug.com/i/c/k/chancerain.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 70,
+	"temp_metric": 21,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 18,
+	"icon": "cloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/cloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 72,
+	"temp_metric": 22,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 16,
+	"icon": "mostlycloudy",
+	"icon_url": "http://icons.wxug.com/i/c/k/mostlycloudy.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 73,
+	"temp_metric": 23,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 3,
+	"icon": "clear",
+	"icon_url": "http://icons.wxug.com/i/c/k/nt_clear.gif",
+	"mday": 2,
+	"mon": 6,
+	"temp_english": 59,
+	"temp_metric": 15,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 4,
+	"icon": "chancerain",
+	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 60,
+	"temp_metric": 16,
+	"year": 2017
+  },
+  {
+	"_index": "london",
+	"hour": 3,
+	"icon": "chancerain",
+	"icon_url": "http://icons.wxug.com/i/c/k/nt_chancerain.gif",
+	"mday": 3,
+	"mon": 6,
+	"temp_english": 61,
+	"temp_metric": 16,
+	"year": 2017
   }
 ];
 
