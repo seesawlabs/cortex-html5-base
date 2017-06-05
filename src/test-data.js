ebab2e05469140d2a23b0ec65c97272df4747753
Calvin Sauer <calvin.j.sauer@gmail.com>
2017-06-05 13:50:17 -0400

    Add test data

--- src/test-data.js
+++ src/test-data.js
@@ -1,20 +1,111 @@
 /* eslint-disable */
 
-// TODO: Update this file with the test data. The format should be exactly what
-// you get from Silo.
-
-const TEST_DATA = [
+const TEST_DATA = 
+[
   {
-    "_index": "device1",
-    "neighborhood": "West Village"
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "jubilee",
+    "name": "Jubilee",
+    "status": "Good Service"
   },
   {
-    "_index": "device2",
-    "neighborhood": "East Village"
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "bakerloo",
+    "name": "Bakerloo",
+    "status": "Good Service"
   },
   {
-    "_index": "device3",
-    "neighborhood": "Chelsea"
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "piccadilly",
+    "name": "Piccadilly",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "central",
+    "name": "Central",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "northern",
+    "name": "Northern",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "metropolitan",
+    "name": "Metropolitan",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "circle",
+    "name": "Circle",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "victoria",
+    "name": "Victoria",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "tfl-rail",
+    "name": "TfL Rail",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "dlr",
+    "name": "DLR",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "district",
+    "name": "District",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "london-overground",
+    "name": "London Overground",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "waterloo-city",
+    "name": "Waterloo & City",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "tram",
+    "name": "Tram",
+    "status": "Good Service"
+  },
+  {
+    "_index": 1,
+    "asof": "2017-06-05T17:48:08Z",
+    "line_id": "hammersmith-city",
+    "name": "Hammersmith & City",
+    "status": "Good Service"
   }
 ];
 
