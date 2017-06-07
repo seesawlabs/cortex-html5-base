01b230976101197375e976d4de799453a9b6f480
Ivan Morales <ivan@imorales.com>
2017-06-07 10:07:29 -0600

    Adding extra simulator data

---
+++ src/test-data-2.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+// TODO: Update this file with the test data. The format should be exactly what
+// you get from Silo.
+
+const TEST_DATA = [
+  {
+    "_index": "device1",
+    "neighborhood": "West Village"
+  },
+  {
+    "_index": "device2",
+    "neighborhood": "East Village"
+  },
+  {
+    "_index": "device3",
+    "neighborhood": "Chelsea"
+  }
+];
+
+module.exports = TEST_DATA;
