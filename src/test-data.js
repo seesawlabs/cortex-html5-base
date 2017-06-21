59945edf25fefabd34c7602b697b7e4c62010165
Calvin Sauer <calvin.j.sauer@gmail.com>
2017-06-21 12:24:58 -0400

    Opened branch and added test data

--- src/test-data.js
+++ src/test-data.js
@@ -1,20 +1,50 @@
 /* eslint-disable */
 
-// TODO: Update this file with the test data. The format should be exactly what
-// you get from Silo.
-
 const TEST_DATA = [
   {
-    "_index": "device1",
-    "neighborhood": "West Village"
+	"_index": 1,
+	"headline": "Agent: Tiger Woods in a clinic for handling pain medications",
+	"updated": "2017-06-13T15:58:57Z"
   },
   {
-    "_index": "device2",
-    "neighborhood": "East Village"
+	"_index": 1,
+	"headline": "Cheering 1st day of summer? Not in Phoenix when 120 expected",
+	"updated": "2017-06-13T15:58:57Z"
   },
   {
-    "_index": "device3",
-    "neighborhood": "Chelsea"
+	"_index": 1,
+	"headline": "GOP preparing for Senate health care vote, amid differences",
+	"updated": "2017-06-13T15:58:57Z"
+  },
+  {
+	"_index": 1,
+	"headline": "US military shoots down Iranian-made drone in southern Syria",
+	"updated": "2017-06-13T15:58:57Z"
+  },
+  {
+	"_index": 1,
+	"headline": "Producer: No evidence of 'Bachelor in Paradise' misconduct",
+	"updated": "2017-06-13T15:58:57Z"
+  },
+  {
+	"_index": 1,
+	"headline": "Georgia voters streaming to the polls in pricy Congress race",
+	"updated": "2017-06-13T15:58:57Z"
+  },
+  {
+	"_index": 1,
+	"headline": "They're back! Numbers of ticks are high across New England",
+	"updated": "2017-06-13T15:58:57Z"
+  },
+  {
+	"_index": 1,
+	"headline": "After Warmbier's death, US weighs travel ban on North Korea",
+	"updated": "2017-06-13T15:58:57Z"
+  },
+  {
+	"_index": 1,
+	"headline": "Public funeral planned for US student freed from North Korea",
+	"updated": "2017-06-13T15:58:57Z"
   }
 ];
 
