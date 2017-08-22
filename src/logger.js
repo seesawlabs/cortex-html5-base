d25ce31e43219813f0e3d3c06b6316051702be4e
Calvin Sauer <calvin.j.sauer@gmail.com>
2017-08-22 13:02:52 -0400

    Add Logger.error static method

--- src/logger.js
+++ src/logger.js
@@ -8,6 +8,14 @@ class Logger {
       console.log(`${APP_NAME}-${new Date().getTime()}: ${msg}`);
     }
   }
+
+  static error(msg, ...args) {
+    if (args && args.length > 0) {
+      console.error(`${APP_NAME}-${new Date().getTime()}: ${msg}`, ...args);
+    } else {
+      console.error(`${APP_NAME}-${new Date().getTime()}: ${msg}`);
+    }
+  }
 }
 
 module.exports = Logger;
